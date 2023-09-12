966568330c487d40e579f4727f494e37e2780d8e
chvaldez10 <christianvaldez021@gmail.com>
2023-09-12 12:03:24 -0600

    finished ex b

--- assignments/labs/lab1/base/lab1exe_B.cpp
+++ assignments/labs/lab1/base/lab1exe_B.cpp
@@ -2,10 +2,11 @@
  *  lab1exe_B.cpp
  *  ENSF 614 Lab 1, exercise B
  *  Created by Mahmood Moussavi
- *  Completed by: Student Name
+ *  Completed by: Christian Valdez
  */
 
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 
@@ -17,31 +18,59 @@ double Projectile_travel_time(double a, double v);
 double Projectile_travel_distance(double a, double v);
 double degree_to_radian(double d);
 
-int main(void)
-{
+double angleDeg;
+double angleRad;
+
+int main(void) {
     double velocity;
-    
+
     cout << "Please enter the velocity at which the projectile is launched (m/sec): ";
     cin >> velocity;
-    
-    if(!cin)  // means if cin failed to read
-    {
+
+    if (!cin) {
         cout << "Invlid input. Bye...\n";
         exit(1);
     }
-    
-    while (velocity < 0 )
-    {
+
+    while (velocity < 0) {
         cout << "\nplease enter a positive number for velocity: ";
         cin >> velocity;
-        if(!cin)
-        {
+        if (!cin) {
             cout << "Invlid input. Bye...";
             exit(1);
         }
     }
-    
+
+    create_table(velocity);
     return 0;
 }
 
+void create_table(double v) {
+    angleDeg = 0;
+    double t;
+    double d;
 
+    cout << "Angle (deg)" << "\t t (sec)" << "\t d (m)" << endl;
+    cout << fixed << setprecision(6);
+
+    while (angleDeg < 15) {
+        angleRad = degree_to_radian(angleDeg);
+        t = Projectile_travel_time(G, v);
+        d = Projectile_travel_distance(G, v);
+
+        cout << angleDeg << "\t " << t << "\t " << d << endl;
+        angleDeg += 5;
+    }
+}
+
+double degree_to_radian(double d) {
+    return angleDeg * (PI/180);
+}
+
+double Projectile_travel_time(double a, double v) {
+    return (2 * v * sin(angleRad)) / a;
+}
+
+double Projectile_travel_distance(double a, double v) {
+    return (v * v * sin(2 * angleRad)) / a;
+}
--- assignments/labs/lab1/lab1/lab1/main.cpp
+++ assignments/labs/lab1/lab1/lab1/main.cpp
@@ -1,15 +1,76 @@
+/*
+ *  lab1exe_B.cpp
+ *  ENSF 614 Lab 1, exercise B
+ *  Created by Mahmood Moussavi
+ *  Completed by: Christian Valdez
+ */
+
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
-int main() {
-	int a = 0, b = 0;
-	cout << "Please enter a value for variable a:" << endl;
-	cin >> a;
+const double G = 9.8;   /* gravitation acceleration 9.8 m/s^2 */
+const double PI = 3.141592654;
 
-	cout << "Please enter a value for variable b:" << endl;
-	cin >> b;
+void create_table(double v);
+double Projectile_travel_time(double a, double v);
+double Projectile_travel_distance(double a, double v);
+double degree_to_radian(double d);
 
-	cout << "The values of a and b are: " << a << " for a, and " << b << " for b." << endl;
-	cout << "The value of " << a << " % " << b << " is " << a % b << endl;
-	return 0;
+double angleDeg;
+double angleRad;
+
+int main(void) {
+    double velocity;
+
+    cout << "Please enter the velocity at which the projectile is launched (m/sec): ";
+    cin >> velocity;
+
+    if (!cin) {
+        cout << "Invlid input. Bye...\n";
+        exit(1);
+    }
+
+    while (velocity < 0) {
+        cout << "\nplease enter a positive number for velocity: ";
+        cin >> velocity;
+        if (!cin) {
+            cout << "Invlid input. Bye...";
+            exit(1);
+        }
+    }
+
+    create_table(velocity);
+    return 0;
+}
+
+void create_table(double v) {
+    angleDeg = 0;
+    double t;
+    double d;
+
+    cout << "Angle (deg)" << "\t t (sec)" << "\t d (m)" << endl;
+    cout << fixed << setprecision(6);
+
+    while (angleDeg < 15) {
+        angleRad = degree_to_radian(angleDeg);
+        t = Projectile_travel_time(G, v);
+        d = Projectile_travel_distance(G, v);
+
+        cout << angleDeg << "\t " << t << "\t " << d << endl;
+        angleDeg += 5;
+    }
+}
+
+double degree_to_radian(double d) {
+    return angleDeg * (PI/180);
+}
+
+double Projectile_travel_time(double a, double v) {
+    return (2 * v * sin(angleRad)) / a;
+}
+
+double Projectile_travel_distance(double a, double v) {
+    return (v * v * sin(2 * angleRad)) / a;
 }
