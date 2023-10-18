e3a8e8b566b8fc609c29afcf64b4aca40f739058
chvaldez10 <christianvaldez021@gmail.com>
2023-10-18 16:25:49 -0600

    added skeleton functions to shape

--- labs/lab5/lab5/lab5/shape.cpp
+++ labs/lab5/lab5/lab5/shape.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 #include <cmath>
+#include <cstring>
 #include "shape.h"
 
+using namespace std;
+
 Shape::Shape(double x, double y, const char* s) : p(x, y) {
-	shapeName = s;
+	int stringLength = strlen(s)+1;
+	shapeName = new char[stringLength];
+	strcpy_s(shapeName, stringLength, s);
+}
+
+//Shape::Shape& (const Shape& shapeSource) : p(shapeSource) {
+//`	shapeName = new char[]
+//}
+
+Shape::~Shape() {
+	delete[] shapeName;
+}
+
+const Point& Shape::getOrigin() const {
+	return p;
+}
+
+const char* Shape::getName() const {
+	return shapeName;
+}
+
+void Shape::display() {
+	cout << "Shape name: " << shapeName <<
+		"X-coordinate: " << p.getX() <<
+		"Y-coordinate: " << p.getY() << endl;
 }
