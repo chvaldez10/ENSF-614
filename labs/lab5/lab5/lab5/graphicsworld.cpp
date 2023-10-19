b6b576d3234e3829b44e74f7569e92a6055cca63
chvaldez10 <christianvaldez021@gmail.com>
2023-10-19 09:55:07 -0600

    added distance() and move() for shape

--- labs/lab5/lab5/lab5/graphicsworld.cpp
+++ labs/lab5/lab5/lab5/graphicsworld.cpp
@@ -1,13 +1,14 @@
 #include "GraphicsWorld.h"
 #include <iostream>
 #include "Point.h"
+#include "Shape.h"
 //#include "Square.h"
 //#include "Rectangle.h"
 
 using namespace std;
 
 void GraphicsWorld::run() {
-#if 1
+#if 0
 	Point m(6, 8);
 	Point n(6, 8);
 	n.setX(9);
@@ -16,6 +17,15 @@ void GraphicsWorld::run() {
 	cout << "\nExpected second version of the distance function also prints: 3";
 	cout << "\nThe distance between m and n is again: " << Point::distance(m, n);
 #endif 
+
+#if 1
+	Shape shape1(6, 8, "shape1");
+	Shape shape2(9, 8, "shape2");
+	shape1.move(6, 9);
+	cout << "testing move," << endl;
+	shape1.display();
+
+#endif
 
 #if 0 // Change 0 to 1 to test Square
 // Assuming the existence of Square class and its methods
