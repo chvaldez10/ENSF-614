6aba171e7eb5f1dd026e3c87df4005dad90a000e
BradenTink <71302948+BradenTink@users.noreply.github.com>
2023-11-11 10:22:13 -0700

    Update Component.java

--- labs/lab6/base/exE_F/Component.java
+++ labs/lab6/base/exE_F/Component.java
@@ -1,3 +1,10 @@
+/**
+ 
+Filename.java
+ENSF 614 Fall 2023 LAB 6 - EXERCISE E and F
+@authors Braden Tink and Christian Valdez
+Submitted On: Nov 11, 2023
+*/
 import java.awt.Graphics;
 
 interface Component {
