120a3d5d66ad8d997a5b9c3be24c758044b357f8
BradenTink <71302948+BradenTink@users.noreply.github.com>
2023-11-11 10:12:33 -0700

    Update Observer.java
    added header

--- labs/lab6/base/exD/Observer.java
+++ labs/lab6/base/exD/Observer.java
@@ -1,3 +1,10 @@
+/**
+ 
+Filename.java
+ENSF 614 Fall 2023 LAB 6 - EXERCISE B and C
+@authors Braden Tink and Christian Valdez
+Submitted On: Nov 11, 2023
+*/
 import java.util.ArrayList;
 
 interface Observer{
