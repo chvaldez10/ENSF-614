0ad4829950b14cc528e724994803cfe97b8b69ed
BradenTink <71302948+BradenTink@users.noreply.github.com>
2023-11-11 12:39:33 -0700

    Update Component.java
    Comments

--- labs/lab6/base/exE_F/Component.java
+++ labs/lab6/base/exE_F/Component.java
@@ -1,12 +1,18 @@
 /**
- 
-Filename.java
-ENSF 614 Fall 2023 LAB 6 - EXERCISE E and F
-@authors Braden Tink and Christian Valdez
-Submitted On: Nov 11, 2023
-*/
+ * Component.java
+ * ENSF 614 Fall 2023 LAB 6 - EXERCISE E and F
+ * @authors Braden Tink and Christian Valdez
+ * Submitted On: Nov 11, 2023
+ */
 import java.awt.Graphics;
 
+/***
+ * Component class that contains one function 
+ * being the draw function that has one argument being a
+ * graphic
+ * @authors Braden Tink and Christian Valdez
+ *
+ */
 interface Component {
 
 
