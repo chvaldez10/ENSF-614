63961d0ec3e962d30539188af129781c2628bb2f
BradenTink <71302948+BradenTink@users.noreply.github.com>
2023-11-11 12:36:26 -0700

    Update Subject.java
    Comments

--- labs/lab6/base/exD/Subject.java
+++ labs/lab6/base/exD/Subject.java
@@ -1,10 +1,20 @@
 /**
- 
-Filename.java
-ENSF 614 Fall 2023 LAB 6 - EXERCISE D
-@authors Braden Tink and Christian Valdez
-Submitted On: Nov 11, 2023
+* Subject.java
+* ENSF 614 Fall 2023 LAB 6 - EXERCISE D
+* @authors Braden Tink and Christian Valdez
+* Submitted On: Nov 11, 2023
 */
+
+/**
+ * Subject is an interface class
+* @authors Braden Tink and Christian Valdez
+ * 
+ * interface has three functions 
+ * 	register 
+ * 	remove
+ *  notifyObservers
+ *
+ */
 interface Subject {
 	
 	public void register(Observer o);
