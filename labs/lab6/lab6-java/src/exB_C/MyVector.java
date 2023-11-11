6106ee41e8ddcd66c720e7514398ed9b988611f6
chvaldez10 <christianvaldez021@gmail.com>
2023-11-11 07:53:22 -0700

    added doc to MyVector

--- labs/lab6/lab6-java/src/exB_C/MyVector.java
+++ labs/lab6/lab6-java/src/exB_C/MyVector.java
@@ -1,36 +1,68 @@
 package exB_C;
 import java.util.ArrayList;
-import java.util.Arrays;
 
+/**
+ * Represents a generic vector that stores elements of type {@link Item}. It allows for sorting
+ * the elements using different sorting strategies defined by the {@link Sorter} interface.
+ *
+ * @param <E> the type of the elements, extending both Number and Comparable
+ */
 public class MyVector<E extends Number & Comparable<E>>{
     private ArrayList<Item<E>> storageM;
     private Sorter<E> sorter;
 
+    /**
+     * Constructs a MyVector with an initial capacity.
+     *
+     * @param size the initial capacity of the vector
+     */
     public MyVector(int size) {
-        storageM = new ArrayList<Item<E>>(size);
+        storageM = new ArrayList<>(size);
     }
 
+    /**
+     * Constructs a MyVector using elements from an existing ArrayList.
+     *
+     * @param arrList the ArrayList containing items to be added to the vector
+     */
     public MyVector(ArrayList<Item<E>> arrList) {
-        storageM = new ArrayList<Item<E>>(arrList.size());
+        storageM = new ArrayList<>(arrList.size());
         for(Item<E> item : arrList) {
             storageM.add(item);
         }
     }
-
-    public void add(Item item) {
+    /**
+     * Adds an item to the vector.
+     *
+     * @param item the item to be added
+     */
+    public void add(Item<E> item) {
         storageM.add(item);
     }
 
+    /**
+     * Sets the sorting strategy for the vector.
+     *
+     * @param s the sorting strategy to be used
+     */
     public void setSortStrategy(Sorter<E> s) {
         sorter = s;
     }
 
+    /**
+     * Sorts the vector according to the currently set sorting strategy.
+     */
     public void performSort() {
-        sorter.sort(storageM);
+        if (sorter != null) {
+            sorter.sort(storageM);
+        }
     }
 
+    /**
+     * Displays the elements of the vector.
+     */
     public void display() {
-        for (Item item : storageM) {
+        for (Item<E> item : storageM) {
             System.out.print(item.getItem() + " ");
         }
         System.out.println();
