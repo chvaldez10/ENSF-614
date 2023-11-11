74a4319fb097a79fb22193810b5b138a99921cb8
chvaldez10 <christianvaldez021@gmail.com>
2023-11-11 07:56:22 -0700

    added doc to selection sort

--- labs/lab6/lab6-java/src/exB_C/SelectionSorter.java
+++ labs/lab6/lab6-java/src/exB_C/SelectionSorter.java
@@ -2,13 +2,18 @@ package exB_C;
 import java.util.ArrayList;
 
 /**
- * The BubbleSorter class provides a method to sort an array list of generic Items
- * using the selection sort algorithm.
+ * The SelectionSorter class provides a method to sort an array list of generic Items
+ * using the selection sort algorithm. It implements the Sorter interface to provide
+ * this sorting strategy.
+ *
+ * @param <E> the type of the elements in the items, extending both Number and Comparable
  */
-
 public class SelectionSorter<E extends Number & Comparable<E>> implements Sorter<E> {
+
     /**
-     * Sorts an array list of Items using selection sort.
+     * Sorts an array list of Items using the selection sort algorithm.
+     * This method iterates over the array list, finds the minimum element from the unsorted
+     * part, and places it at the beginning. The process is repeated until the whole array is sorted.
      *
      * @param arr the array list of Items to be sorted
      */
@@ -17,20 +22,20 @@ public class SelectionSorter<E extends Number & Comparable<E>> implements Sorter
 
         for (int i = 0; i < n - 1; i++) {
             // Find the minimum element in the unsorted part of the array
-            int min_idx = i;
+            int minIdx = i;
             for (int j = i + 1; j < n; j++) {
                 E current = arr.get(j).getItem();
-                E min = arr.get(min_idx).getItem();
+                E min = arr.get(minIdx).getItem();
 
                 // Compare current element with the minimum found so far
                 if (current.compareTo(min) < 0) {
-                    min_idx = j;
+                    minIdx = j;
                 }
             }
 
             // Swap the found minimum element with the first element
-            Item<E> temp = arr.get(min_idx);
-            arr.set(min_idx, arr.get(i));
+            Item<E> temp = arr.get(minIdx);
+            arr.set(minIdx, arr.get(i));
             arr.set(i, temp);
         }
     }
--- labs/lab6/lab6-java/src/exB_C/Sorter.java
+++ labs/lab6/lab6-java/src/exB_C/Sorter.java
@@ -1,6 +1,20 @@
 package exB_C;
+
 import java.util.ArrayList;
 
+/**
+ * Defines the sorting behavior for objects of type {@link Item}.
+ * This interface is part of the strategy pattern and allows for different sorting
+ * implementations for a collection of {@link Item} objects.
+ *
+ * @param <E> the type of the elements in the items, extending both Number and Comparable
+ */
 public interface Sorter<E extends Number & Comparable<E>> {
+
+    /**
+     * Sorts the provided ArrayList of {@link Item} objects.
+     *
+     * @param arr the ArrayList of {@link Item} objects to be sorted
+     */
     void sort(ArrayList<Item<E>> arr);
 }
