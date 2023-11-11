fff933bf6e32ceebb2c0a9cfb05caa49165875c3
BradenTink <71302948+BradenTink@users.noreply.github.com>
2023-11-11 12:36:39 -0700

    Update OneRow_Observer.java
    comments

--- labs/lab6/base/exD/OneRow_Observer.java
+++ labs/lab6/base/exD/OneRow_Observer.java
@@ -1,17 +1,26 @@
 /**
- 
-Filename.java
-ENSF 614 Fall 2023 LAB 6 - EXERCISE D
-@authors Braden Tink and Christian Valdez
-Submitted On: Nov 11, 2023
+* OneRow_Observer.java
+* ENSF 614 Fall 2023 LAB 6 - EXERCISE D
+* @authors Braden Tink and Christian Valdez
+* Submitted On: Nov 11, 2023
 */
+
 import java.util.ArrayList;
 
+/**
+ * OneRow_Observer implements observer 
+* @authors Braden Tink and Christian Valdez
+ * Has one class varialbe which of type subject 
+ */
 public class OneRow_Observer implements Observer {
 
 	Subject table;
 	
-	
+	/**
+	 * Constructor of the class which takes one argument of type subject 
+	 * Then sets the table class variable 
+	 * @param tbl
+	 */
 	public OneRow_Observer(Subject tbl) {
 		// TODO Auto-generated constructor stub
 		this.table = tbl;
@@ -19,7 +28,12 @@ public class OneRow_Observer implements Observer {
 	}
 
 	
-	
+	/**
+	 * update function which takes one argument being the arraylist data 
+	 * array
+	 * 
+	 * Function then calls update passing in the data array 
+	 */
 	@Override
 	public void update(ArrayList<Double> data) {
 		// TODO Auto-generated method stub
@@ -27,7 +41,11 @@ public class OneRow_Observer implements Observer {
 	}
 	
 	
-
+	/**
+	 * Display function displays the data of the array list 
+	 * 
+	 * @param data
+	 */
 	public void display(ArrayList<Double> data) {
 		System.out.println("\nNotification to One-Column Table Observer: Data Changed:");
 		
