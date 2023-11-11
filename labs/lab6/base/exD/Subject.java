ebb796079581d8bf1b618a58a2d5d89601069fd7
BradenTink <71302948+BradenTink@users.noreply.github.com>
2023-11-11 10:16:51 -0700

    Update Subject.java
    Added Header

--- labs/lab6/base/exD/Subject.java
+++ labs/lab6/base/exD/Subject.java
@@ -1,4 +1,10 @@
-
+/**
+ 
+Filename.java
+ENSF 614 Fall 2023 LAB 6 - EXERCISE D
+@authors Braden Tink and Christian Valdez
+Submitted On: Nov 11, 2023
+*/
 interface Subject {
 	
 	public void register(Observer o);
