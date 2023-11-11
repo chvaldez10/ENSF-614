f5afd6216eedae160e836a59b96edcd7e6f01ab7
chvaldez10 <christianvaldez021@gmail.com>
2023-11-11 07:58:52 -0700

    added doc to bubble and insertion sort

--- labs/lab6/lab6-java/src/exB_C/BubbleSorter.java
+++ labs/lab6/lab6-java/src/exB_C/BubbleSorter.java
@@ -3,11 +3,18 @@ import java.util.ArrayList;
 
 /**
  * The BubbleSorter class provides a method to sort an array list of generic Items
- * using the bubble sort algorithm.
+ * using the bubble sort algorithm. It implements the Sorter interface to provide
+ * this sorting strategy.
+ *
+ * @param <E> the type of the elements in the items, extending both Number and Comparable
  */
 public class BubbleSorter<E extends Number & Comparable<E>> implements Sorter<E> {
+
     /**
-     * Sorts an array list of Items using bubble sort.
+     * Sorts an array list of Items using the bubble sort algorithm. In each iteration,
+     * adjacent items are compared and swapped if they are in the wrong order, thus
+     * "bubbling" the highest or lowest value to the top of the list, depending on the
+     * order of sorting.
      *
      * @param arr the array list of Items to be sorted
      */
@@ -18,8 +25,9 @@ public class BubbleSorter<E extends Number & Comparable<E>> implements Sorter<E>
             for (int j = 0; j < n - i - 1; j++) {
                 Item<E> currentItem = arr.get(j);
                 Item<E> nextItem = arr.get(j + 1);
+
+                // Swap items if they are in the wrong order
                 if (currentItem.getItem().compareTo(nextItem.getItem()) > 0) {
-                    // swap items in the array list
                     arr.set(j, nextItem);
                     arr.set(j + 1, currentItem);
                 }
--- labs/lab6/lab6-java/src/exB_C/InsertionSorter.java
+++ labs/lab6/lab6-java/src/exB_C/InsertionSorter.java
@@ -2,14 +2,20 @@ package exB_C;
 import java.util.ArrayList;
 
 /**
- * This class provides a static method to sort an array of integers using
- * the insertion sort algorithm.
+ * The InsertionSorter class provides a method to sort an ArrayList of generic Items
+ * using the insertion sort algorithm. It implements the Sorter interface to provide
+ * this sorting strategy.
+ *
+ * @param <E> the type of the elements in the items, extending both Number and Comparable
  */
 public class InsertionSorter<E extends Number & Comparable<E>> implements Sorter<E> {
+
     /**
-     * Sorts an ArrayList of Items in ascending order using insertion sort.
+     * Sorts an ArrayList of Items in ascending order using the insertion sort algorithm.
+     * This method iterates over the elements, and for each, it finds the correct position
+     * in the already sorted part of the list, placing it there.
      *
-     * @param arr The ArrayList of Items to be sorted.
+     * @param arr The ArrayList of Items to be sorted
      */
     @Override
     public void sort(ArrayList<Item<E>> arr) {
@@ -17,7 +23,7 @@ public class InsertionSorter<E extends Number & Comparable<E>> implements Sorter
             Item<E> currentItem = arr.get(i);
             int j = i - 1;
 
-            // Use Item.getItem() to compare the values within the Items
+            // Move elements greater than currentItem one position ahead of their current position
             while (j >= 0 && arr.get(j).getItem().compareTo(currentItem.getItem()) > 0) {
                 arr.set(j + 1, arr.get(j));
                 j--;
